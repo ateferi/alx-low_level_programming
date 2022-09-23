28eaeebc7160f4117981433719cfb9e1531d4f35
AbebeTeferi Lemma <abebeteferi.teferi@gmail.com>
2022-09-23 05:49:29 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,13 +15,13 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-  {
+  	{
 		putchar(letter);
-  }
+  	}
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
-    putchar(letter);
-  }
+    		putchar(letter);
+  	}
 
 	putchar('\n');
 
