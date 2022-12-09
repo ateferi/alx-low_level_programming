e4eff1f046b3c473f4f42e7c0c30d8ac02497756
ateferi <abebeteferi.teferi@gmail.com>
2022-12-08 23:49:29 -0800

    changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,13 @@ int main(void)
 {
 	int n;
 	srand(time(0));
-	n = rand() -RAND_MAX /2;
+
+
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
@@ -29,7 +31,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
