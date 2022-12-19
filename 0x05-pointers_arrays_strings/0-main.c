21e42cc69460e77730087919d59a6f3db153f55b
ateferi <abebeteferi.teferi@gmail.com>
2022-12-19 00:00:02 -0800

    changes

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -3,7 +3,6 @@
 
 /**
  * main-check the code
- * 
  * Return: Always 0.
  */
 int main(void)
@@ -14,5 +13,5 @@ int main(void)
 	printf("n=%d\n", n);
 	reset_to_98(&n);
 	printf("n=%d\n", n);
-	return 0;
+	return (0);
 }
